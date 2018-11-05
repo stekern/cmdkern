ef915e16de96112f7331dcebd30d67382d12783a
stekern <stekern@users.noreply.github.com>
2018-11-05 13:28:27 +0100

    Add line prefix and support for leading spaces

--- src/components/Linewriter.js
+++ src/components/Linewriter.js
@@ -4,6 +4,10 @@ import { getFormattedLine, removeTags } from '../utils/utils'
 class Linewriter extends React.Component {
   timeoutId = undefined
 
+  linePrefix = getFormattedLine(
+    '[green][bold]➜[/bold][/green] [blue][bold]~[/bold][/blue] '
+  )
+
   state = {
     charIndex: 0,
   }
@@ -13,13 +17,26 @@ class Linewriter extends React.Component {
     this.timeoutId = setInterval(() => this.nextLetter(), speed)
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
     const { handleFinish, line, speed } = this.props
+    const { charIndex } = this.state
     if (speed === 0) {
       handleFinish(getFormattedLine(line))
-    } else if (speed !== prevProps.speed) {
+    } else if (!line.length) {
+      clearInterval(this.timeoutId)
+      this.timeoutId = setInterval(() => this.nextLetter(), 200)
+    } else if (!prevProps.line.length && charIndex === 0) {
       clearInterval(this.timeoutId)
       this.timeoutId = setInterval(() => this.nextLetter(), speed)
+    } else if (
+      speed !== prevProps.speed ||
+      prevState.charIndex === prevProps.line.length - 1
+    ) {
+      clearInterval(this.timeoutId)
+      this.timeoutId = setInterval(() => this.nextLetter(), speed)
+    } else if (charIndex === line.length - 1) {
+      clearInterval(this.timeoutId)
+      this.timeoutId = setInterval(() => this.nextLetter(), 0)
     }
   }
 
@@ -33,13 +50,42 @@ class Linewriter extends React.Component {
     const prefixedLine = `${prefix ? '> ' : ''}${line}`
 
     if (!line.length) {
-      handleFinish(getFormattedLine(prefixedLine))
+      handleFinish(
+        <React.Fragment>
+          {prefix && this.linePrefix}
+          {line.split(/^(\s*)/).length === 3 &&
+            line
+              .split(/^(\s*)/)[1]
+              .split('')
+              .map(_ => <span>&nbsp;</span>)}
+          {getFormattedLine(line)}
+        </React.Fragment>
+      )
     } else if (charIndex < line.length - 1) {
       this.setState(prevState => ({
         charIndex: prevState.charIndex + 1,
         ...{
           ...(charIndex === 0 && {
-            formattedLine: getFormattedLine(prefixedLine),
+            formattedLine: prefix ? (
+              <React.Fragment>
+                {this.linePrefix}
+                {line.split(/^(\s*)/).length === 3 &&
+                  line
+                    .split(/^(\s*)/)[1]
+                    .split('')
+                    .map(_ => <span>&nbsp;</span>)}
+                {getFormattedLine(line)}
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {line.split(/^(\s*)/).length === 3 &&
+                  line
+                    .split(/^(\s*)/)[1]
+                    .split('')
+                    .map(_ => <span>&nbsp;</span>)}
+                {getFormattedLine(line)}
+              </React.Fragment>
+            ),
           }),
         },
       }))
@@ -56,10 +102,17 @@ class Linewriter extends React.Component {
     const { charIndex } = this.state
     const { cursor, line, prefix } = this.props
     const outputLine = removeTags(line)
+    const leadingSpaces = outputLine.split(/^(\s*)/).length === 3
     // const outputLine = line
     return (
       <div className="terminal__line">
-        {`${prefix ? '> ' : ''}${outputLine.substr(0, charIndex)}`}
+        {prefix && this.linePrefix}
+        {leadingSpaces &&
+          outputLine
+            .split(/^(\s*)/)[1]
+            .split('')
+            .map(_ => <span>&nbsp;</span>)}
+        {`${outputLine.substr(0, charIndex)}`}
         {cursor && <span className="cursor">&nbsp;</span>}
       </div>
     )
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,83 +1,100 @@
 import * as React from 'react'
 
-const getFormattedLine = line => {
-  const allowedTags = [
-    'bold',
-    'blue',
-    'email',
-    'green',
-    'italic',
-    'underscore',
-    'url',
-  ]
-  let formattedLine = line
-  for (let i = 0; i < allowedTags.length; i++) {
-    const tag = allowedTags[i]
+const ALLOWED_TAGS = [
+  'bold',
+  'email',
+  'green',
+  'blue',
+  'italic',
+  'underscore',
+  'url',
+]
+
+const hasTags = line => {
+  for (let i = 0; i < ALLOWED_TAGS.length; i++) {
+    const tag = ALLOWED_TAGS[i]
     const regex = new RegExp(`^(.*)(\\[${tag}\\])(.*)(\\[/${tag}\\])(.*)$`)
     const matches = line.match(regex)
     if (matches && matches.length === 6) {
+      return true
+    }
+  }
+  return false
+}
+
+const getFormattedLine = line => {
+  let formattedLine = line
+  for (let i = 0; i < ALLOWED_TAGS.length; i++) {
+    const tag = ALLOWED_TAGS[i]
+    const regex = new RegExp(`^(.*)(\\[${tag}\\])(.*)(\\[/${tag}\\])(.*)$`)
+    const matches = line.match(regex)
+    if (matches && matches.length === 6) {
+      const start = getFormattedLine(matches[1])
+      const mid = getFormattedLine(matches[3])
+      const end = getFormattedLine(matches[5])
+
       if (tag === 'bold') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
-            <strong>{matches[3]}</strong>
-            {matches[5]}
+            {start}
+            <strong>{mid}</strong>
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'url') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
+            {start}
             <a rel="noopener noreferrer" target="_blank" href={matches[3]}>
-              {matches[3]}
+              {mid}
             </a>
-            {matches[5]}
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'email') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
+            {start}
             <a
               rel="noopener noreferrer"
               target="_blank"
               href={`mailto:${matches[3]}`}
             >
-              {matches[3]}
+              {mid}
             </a>
-            {matches[5]}
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'italic') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
-            <i>{matches[3]}</i>
-            {matches[5]}
+            {start}
+            <i>{mid}</i>
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'underscore') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
-            <u>{matches[3]}</u>
-            {matches[5]}
+            {start}
+            <u>{mid}</u>
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'green') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
-            <font color="#20C20E">{matches[3]}</font>
-            {matches[5]}
+            {start}
+            <font color="#20C20E">{mid}</font>
+            {end}
           </React.Fragment>
         )
       } else if (tag === 'blue') {
         formattedLine = (
           <React.Fragment>
-            {matches[1]}
-            <font color="#3399ff">{matches[3]}</font>
-            {matches[5]}
+            {start}
+            <font color="#3399ff">{mid}</font>
+            {end}
           </React.Fragment>
         )
       }
@@ -87,18 +104,9 @@ const getFormattedLine = line => {
 }
 
 const removeTags = line => {
-  const allowedTags = [
-    'bold',
-    'blue',
-    'email',
-    'green',
-    'italic',
-    'underscore',
-    'url',
-  ]
   let formattedLine = line
-  for (let i = 0; i < allowedTags.length; i++) {
-    const tag = allowedTags[i]
+  for (let i = 0; i < ALLOWED_TAGS.length; i++) {
+    const tag = ALLOWED_TAGS[i]
     const regex = new RegExp(`\\[/?${tag}\\]`, 'gi')
     formattedLine = formattedLine.replace(regex, '')
   }
@@ -107,4 +115,4 @@ const removeTags = line => {
 
 const getElementKey = prefix => `${prefix}_${new Date().getTime()}`
 
-export { getElementKey, getFormattedLine, removeTags }
+export { getElementKey, getFormattedLine, hasTags, removeTags }
