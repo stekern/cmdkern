e0955b0cd3775759a4eb903ae447f68810a972da
stekern <stekern@users.noreply.github.com>
2021-01-31 19:23:42 +0100

    Rewrite with vanilla JavaScript

---
+++ src/main.js
@@ -0,0 +1,90 @@
+import { hasTags, getDisplayText, getFormattedText } from './util.js'
+import sections from './assets/json/sections.json'
+
+// Defaults
+const CHAR_DELAY = 20
+const START_DELAY = 0
+const END_DELAY = 300
+const SHOW_PROMPT = true
+const SHOW_CURSOR = true
+const MIN_SCREEN_WIDTH = Number.NEGATIVE_INFINITY
+const PROMPT = "<font color='white'><strong>🚀 ~  </strong>"
+
+// On load
+document.addEventListener('DOMContentLoaded', (e) => {
+  const terminal = document.getElementById('terminal')
+  const screenWidth = window.innerWidth
+  const formattedLines = sections
+    .filter(
+      (section) => screenWidth >= (section.minScreenWidth ?? MIN_SCREEN_WIDTH)
+    )
+    .map((section) =>
+      section.lines.map((line) => ({
+        ...section,
+        displayText: line,
+        formattedText:
+          line || (section.showPrompt ?? SHOW_PROMPT) ? line : '&nbsp',
+        ...(hasTags(line)
+          ? {
+              displayText: getDisplayText(line),
+              formattedText: getFormattedText(line),
+            }
+          : {}),
+      }))
+    )
+    .flat(1)
+  const writeLine = (i) => {
+    if (formattedLines[i]) {
+      const paragraph = document.createElement('p')
+      paragraph.innerHTML =
+        formattedLines[i].showPrompt ?? SHOW_PROMPT
+          ? formattedLines[i].prompt ?? PROMPT
+          : ''
+      terminal.appendChild(paragraph)
+      const writeChar = (j) => {
+        if (formattedLines[i].displayText[j] || j === 0) {
+          if (formattedLines[i].showCursor ?? SHOW_CURSOR) {
+            paragraph.innerHTML =
+              paragraph.innerHTML.replace('<span class="cursor"></span>', '') +
+              (formattedLines[i].displayText[j] || '') +
+              '<span class="cursor"></span>'
+          } else {
+            paragraph.innerHTML += formattedLines[i].displayText[j] || ''
+          }
+          setTimeout(
+            () => writeChar(j + 1),
+            formattedLines[i].charDelay ?? CHAR_DELAY
+          )
+        } else {
+          setTimeout(() => {
+            if (formattedLines[i].showCursor ?? SHOW_CURSOR) {
+              paragraph.innerHTML = paragraph.innerHTML
+                .replace('<span class="cursor"></span>', '')
+                .replace(
+                  formattedLines[i].displayText,
+                  formattedLines[i].formattedText
+                )
+            } else {
+              paragraph.innerHTML = paragraph.innerHTML.replace(
+                formattedLines[i].displayText,
+                formattedLines[i].formattedText
+              )
+            }
+            writeLine(i + 1)
+          }, (formattedLines[i].startDelay ?? START_DELAY) + (formattedLines[i].endDelay ?? END_DELAY))
+        }
+      }
+      if (
+        formattedLines[i].endDelay === 0 &&
+        formattedLines[i].charDelay === 0
+      ) {
+        paragraph.innerHTML =
+          paragraph.innerHTML + formattedLines[i].formattedText
+        writeLine(i + 1)
+      } else {
+        writeChar(0)
+      }
+    }
+  }
+  writeLine(0)
+})
